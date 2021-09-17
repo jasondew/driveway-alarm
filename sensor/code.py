fd124a67e42ea8e52643d057fa792b79078224fb
Jason Dew <jason.dew@gmail.com>
2021-09-16 22:13:07 -0400

    Adding support for setting the time on startup via JSONRPC

--- sensor/code.py
+++ sensor/code.py
@@ -1,24 +1,28 @@
 import board
 import busio
+import json
 import microcontroller
 import neopixel
+import rtc
 import time
 
 from analogio import AnalogIn
+from simpleio import DigitalOut
 
 led = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness=0.05)
-uart = busio.UART(board.TX, board.RX, baudrate=115200, bits=8, parity=None, stop=1, timeout=60)
-a0 = AnalogIn(board.A0)
-a1 = AnalogIn(board.A1)
+uart = busio.UART(board.TX, board.RX, baudrate=115200, bits=8, parity=None, stop=1, timeout=10)
+battery = AnalogIn(board.A0)
+sonar = AnalogIn(board.A1)
+lora_reset = DigitalOut(board.D10)
 
-def read_cpu_temp():
-    return microcontroller.cpu.temperature
+def cpu_temp():
+    return round(microcontroller.cpu.temperature * 10) / 10
 
-def read_voltage():
-    return a0.value / 9773
+def battery_voltage():
+    return battery.value
 
-def read_break_sensor():
-    return a1.value < 32768
+def sonar_voltage():
+    return sonar.value
 
 def led_on():
     led.fill((255, 0, 0))
@@ -26,78 +30,82 @@ def led_on():
 def led_off():
     led.fill((0, 0, 0))
 
-def read_response():
+def reset_lora():
+    print("Resetting LoRa...\n")
+    lora_reset.value = False # active LOW
+    time.sleep(1) # must stay set for at least 100ms
+    lora_reset.value = True
     while True:
-        data = uart.readline()
+        response = get_response()
+        print("reset response: %s" % repr(response))
 
-        if data is not None:
-            print(''.join([chr(b) for b in data]), end="")
-            break
+        if get_response() is None:
+            print("reset successful.")
+            send_data({"event": "reset"})
+            return
 
-def setup_uart():
-    uart.write(b'AT+FACTORY\r\n')
-    time.sleep(0.1)
-    read_response()
+def send_command(command, expected_response="OK"):
+    print("> {}".format(command))
+    command_bytes = bytes(command + "\r\n", "ascii")
+    uart.write(command_bytes)
+    response = get_response()
 
-    uart.write(b'AT+NETWORKID=3\r\n')
-    time.sleep(0.5)
-    read_response()
+    print("< {}".format(response))
 
-    uart.write(b'AT+ADDRESS=1\r\n')
-    time.sleep(0.5)
-    read_response()
+    if not response.endswith("+{}\r\n".format(expected_response)):
+        print("[ERROR] received unexpected response: %s" % repr(response))
+        reset_lora()
+        time.sleep(10)
+        send_command(command)
 
-def send_data(message):
-    print("AT+MODE=0")
-    uart.write(b"AT+MODE=0\r\n")
-    time.sleep(0.5)
-    read_response()
+def get_response():
+    data = uart.readline()
 
-    print("AT+SEND")
-    uart.write(b"AT+SEND=0,%s,%s\r\n" % (len(message), message))
-    time.sleep(0.5)
-    read_response()
+    if data is None:
+        return None
+    else:
+        return "".join([chr(b) for b in data])
 
-    print("AT+MODE=1")
-    uart.write(b"AT+MODE=1\r\n")
-    time.sleep(0.5)
-    read_response()
-    print("\n")
+def send_data(data):
+    payload = json.dumps(data)
+#    send_command("AT+MODE=0") # transmit and receive mode
+    send_command("AT+SEND=0,%s,%s" % (len(payload), payload))
+#    send_command("AT+MODE=1") # sleep mode
 
 def send_telemetry():
-    send_data("{}:{}:{}".format(time.time(), read_voltage(), read_cpu_temp()))
+    send_data({
+        "event": "telemetry",
+        "timestamp": time.time(),
+        "battery_voltage": battery_voltage(),
+        "sonar_voltage": sonar_voltage(),
+        "cpu_temperature": cpu_temp()
+    })
 
-def send_break_message():
-    send_data("{}:BREAK".format(time.time()))
+def setup_uart():
+    reset_lora()
+    send_command("AT+FACTORY", "FACTORY")
+    send_command("AT+NETWORKID=3")
+    send_command("AT+ADDRESS=1")
+    send_data({"event": "startup"})
+    set_clock()
 
-def send_repair_message():
-    send_data("{}:REPAIR".format(time.time()))
+def set_clock():
+    while True:
+        send_data({"jsonrpc": "2.0", "method": "get_time"})
+        response = get_response()
+        if response is not None:
+            break;
+    [_from, _length, rest] = response.split(",", 2)
+    [payload, _rssi, _snr] = rest.rsplit(",", 2)
+    data = json.loads(payload)
+    [year, month, day, hour, minute, second, day_of_week] = list(map(lambda value: int(value), data["result"].split("-")))
+    rtc.RTC().datetime = time.struct_time((year, month, day, hour, minute, second, day_of_week, -1, -1))
 
-# setup
-led_off()
+# ENTRYPOINT
+
 setup_uart()
-index = 0
-break_message_sent = False
-repair_message_sent = False
-
-# loop
 while True:
-    if read_break_sensor():
-        led_on()
-        repair_message_sent = False
-        if not break_message_sent:
-            send_break_message()
-            break_message_sent = True
-    else:
-        led_off()
-        break_message_sent = False
-        if not repair_message_sent:
-            send_repair_message()
-            repair_message_sent = True
-
-    if index >= (30 * 10):
-        send_telemetry()
-        index = 0
-
-    index += 1
-    time.sleep(0.1)
+    led_on()
+    send_telemetry()
+    led_off()
+    time.sleep(10)
