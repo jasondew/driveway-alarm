99842f8e071c368641ba284a5c1b4eda2e29ddf4
Jason Dew <jason.dew@gmail.com>
2021-09-23 21:07:48 -0400

    Don't alert more than once per minute

--- sensor/code.py
+++ sensor/code.py
@@ -19,8 +19,8 @@ battery = AnalogIn(board.A0)
 sonar = AnalogIn(board.A1)
 dht22 = DHT22(board.D4)
 
-def led_on():
-    led.fill((255, 0, 0))
+def led_on(color):
+    led.fill(color)
 
 def led_off():
     led.fill((0, 0, 0))
@@ -51,7 +51,7 @@ def reset_lora():
 
         if get_response() is None:
             print("reset successful.")
-            send_data({"event": "reset"})
+            send_event("reset")
             return
 
 def send_command(command, expected_response="OK"):
@@ -60,7 +60,7 @@ def send_command(command, expected_response="OK"):
     lora.write(command_bytes)
     response = get_response()
 
-    if not response.endswith("+{}\r\n".format(expected_response)):
+    if response is None or not response.endswith("+{}\r\n".format(expected_response)):
         print("[ERROR] received unexpected response: %s" % repr(response))
         reset_lora()
         time.sleep(10)
@@ -82,6 +82,9 @@ def send_data(data):
     send_command("AT+SEND=0,%s,%s" % (len(payload), payload))
 #    send_command("AT+MODE=1") # sleep mode
 
+def send_event(name):
+    send_data({"event": name})
+
 def send_telemetry():
     send_data({
         "event": "telemetry",
@@ -98,7 +101,7 @@ def setup_lora():
     send_command("AT+FACTORY", "FACTORY")
     send_command("AT+NETWORKID=3")
     send_command("AT+ADDRESS=1")
-    send_data({"event": "startup"})
+    send_event("startup")
     set_clock()
 
 def set_clock():
@@ -115,9 +118,32 @@ def set_clock():
 
 # ENTRYPOINT
 
+counter = 0
+triggered = False
+triggered_for = 0
+led_on((0, 255, 0))
 setup_lora()
+
 while True:
-    led_on()
-    send_telemetry()
     led_off()
-    time.sleep(10)
+
+    if triggered:
+        if triggered_for > 60:
+            triggered = False
+            triggered_for = 0
+        else:
+            triggered_for += 1
+    else:
+        if (sonar.value / 1962) < 3.0:
+            led_on((255, 0, 0))
+            send_event("triggered")
+            triggered = True
+
+    if counter >= 60:
+        led_on((0, 0, 255))
+        send_telemetry()
+        counter = 0
+    else:
+        counter += 1
+
+    time.sleep(1)
